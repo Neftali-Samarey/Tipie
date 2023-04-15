a7dd22e4669cd80c4081ad3bc3bc274f43bea340
Neftali-Samarey <neftali.quintero@cix.csi.cuny.edu>
2023-04-15 15:03:59 -0400

    General file creations

---
+++ Tipie/Interface/TipieMainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TipieMainViewController.swift
+//  Tipie
+//
+//  Created by Neftali Samarey on 3/9/23.
+//  Copyright © 2023 Neftali Samarey. All rights reserved.
+//
+
+import UIKit
+
+class TipieMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
